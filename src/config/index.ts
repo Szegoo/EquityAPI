04775abd15ea2e9e79dd075ecfb8424ef631badf
Szegoo <sakacszergej@gmail.com>
2021-12-03 20:36:03 +0100

    cleaned up

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const config = {
-    port: 5001,
-    corsOptions: {
-        origin: "http://127.0.0.1:8080",
-    }
+  port: 5001,
+  corsOptions: {
+    origin: "http://127.0.0.1:8080",
+  },
 };
